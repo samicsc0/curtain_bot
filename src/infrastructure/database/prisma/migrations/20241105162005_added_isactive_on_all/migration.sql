8baeb798bb947309edb2abd24ab453d116e16ae7
samuel zewde <samuelzewde29@gmail.com>
2024-11-05 19:20:51 +0300

    fix: updated schema to have is_active column

---
+++ src/infrastructure/database/prisma/migrations/20241105162005_added_isactive_on_all/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Admin" ADD COLUMN     "is_active" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "Curtain" ADD COLUMN     "is_active" BOOLEAN NOT NULL DEFAULT true;
